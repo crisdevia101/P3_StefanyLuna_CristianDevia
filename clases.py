037230cce12c13bdba597260afe9216b5e0c6023
crisdevia101 <cristian.devia@udea.edu.co>
2025-11-08 19:18:18 -0500

    ultimo error ya ya ya

--- clases.py
+++ clases.py
@@ -7,15 +7,6 @@ import matplotlib.pyplot as plt
 import nibabel as nib
 from datetime import datetime
 
-import os
-import numpy as np
-import pydicom
-import pandas as pd
-import cv2
-import matplotlib.pyplot as plt
-import nibabel as nib
-from datetime import datetime
-
 class GestorDICOM:
     def __init__(self, ruta_carpeta):
         """
